946af8a304e25e78d7cdf7b6d72f6d7823f652be
sranjeit <32730692+sranjeit@users.noreply.github.com>
2018-05-05 21:41:45 +0530

    Add files via upload

--- js/washare.js
+++ js/washare.js
@@ -1,1 +1,25 @@
+$(document).ready(function(){
+    $('form').on("submit",function(){
+        console.log("Loading...");
+    });
+});
 
+
+
+/*
+$(document).ready(function () {
+   $(document).on("click", '.whatsapp', function () {
+      if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        var sText = "Text to share";
+        var sUrl = "Link to share";
+        var sMsg = encodeURIComponent(sText) + " - " + encodeURIComponent(sUrl);
+        var whatsapp_url = "whatsapp://send?text=" + sMsg;
+        window.location.href = whatsapp_url;
+     }
+     else {
+        alert("Whatsapp client not available.");
+     }
+  });
+});
+
+*/
